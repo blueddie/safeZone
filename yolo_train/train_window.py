1be676775df96701b263c7312698386ff0bf5a52
blueddie <blueddie@naver.com>
2024-06-04 17:23:42 +0900

    -

--- yolo_train/train_window.py
+++ yolo_train/train_window.py
@@ -2,11 +2,11 @@ from ultralytics import YOLO
 
 def main():
     # Load a model
-    model = YOLO("yolov8s.yaml")  # build a new model from scratch
-    model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data="data.yaml", epochs=3)  # train the model
+    model.train(data="label8.yaml", epochs=500, batch=100, patience=100)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
     results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
     path = model.export(format="onnx")  # export the model to ONNX format
