d01defaa38118bf185da0546b2e36de35cf894ab
blueddie <blueddie@naver.com>
2024-06-12 13:07:21 +0900

    Update train_window.py

--- yolo_train/train_window.py
+++ yolo_train/train_window.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 def main():
     # Load a model
     model = YOLO("yolov8n.yaml")  # build a new model from scratch
-    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
     model.train(data="label8.yaml", epochs=500, batch=100, patience=100)  # train the model
